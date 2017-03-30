acac0daf5b2f80774ee6cff5c50152d85a8cbac9
Hoan Ngo <nthoan@ipay88.com.my>
2017-03-30 14:10:01 +0700

    initial commit

---
+++ app/src/main/java/my/com/amipay88/editextsample/FourDigitCardFormatWatcher.java
@@ -0,0 +1,71 @@
+package my.com.amipay88.editextsample;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by hoan on 3/29/17
+ */
+public  class FourDigitCardFormatWatcher implements TextWatcher {
+
+    private static final int TOTAL_SYMBOLS = 19; // size of pattern 0000-0000-0000-0000
+    private static final int TOTAL_DIGITS = 16; // max numbers of digits in pattern: 0000 x 4
+    private static final int DIVIDER_MODULO = 5; // means divider position is every 5th symbol beginning with 1
+    private static final int DIVIDER_POSITION = DIVIDER_MODULO - 1; // means divider position is every 4th symbol beginning with 0
+    private static final char DIVIDER = ' ';
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if (!isInputCorrect(s, TOTAL_SYMBOLS, DIVIDER_MODULO, DIVIDER)) {
+            s.replace(0, s.length(), buildCorrecntString(getDigitArray(s, TOTAL_DIGITS), DIVIDER_POSITION, DIVIDER));
+        }
+    }
+
+    private boolean isInputCorrect(Editable s, int totalSymbols, int dividerModulo, char divider) {
+        boolean isCorrect = s.length() <= totalSymbols; // check size of entered string
+        for (int i = 0; i < s.length(); i++) { // chech that every element is right
+            if (i > 0 && (i + 1) % dividerModulo == 0) {
+                isCorrect &= divider == s.charAt(i);
+            } else {
+                isCorrect &= Character.isDigit(s.charAt(i));
+            }
+        }
+        return isCorrect;
+    }
+
+    private String buildCorrecntString(char[] digits, int dividerPosition, char divider) {
+        final StringBuilder formatted = new StringBuilder();
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] != 0) {
+                formatted.append(digits[i]);
+                if ((i > 0) && (i < (digits.length - 1)) && (((i + 1) % dividerPosition) == 0)) {
+                    formatted.append(divider);
+                }
+            }
+        }
+
+        return formatted.toString();
+    }
+
+    private char[] getDigitArray(final Editable s, final int size) {
+        char[] digits = new char[size];
+        int index = 0;
+        for (int i = 0; i < s.length() && index < size; i++) {
+            char current = s.charAt(i);
+            if (Character.isDigit(current)) {
+                digits[index] = current;
+                index++;
+            }
+        }
+        return digits;
+    }
+}
